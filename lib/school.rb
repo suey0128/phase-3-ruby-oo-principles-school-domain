b6a3ec35067e2799cc3300f3ac02744dc0c00560
suey0128 <syui0128@gmail.com>
2021-06-29 17:21:14 -0700

    commit tree f58237ab3c930f81d1758428c9efa88f9541d396

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,36 @@
-# code here!
+require 'pry'
+class School
+    attr_accessor :name, :roster
+
+    def initialize name, roster={}
+        @name = name
+        @roster = roster
+    end 
+
+    def add_student name, grade
+        @roster[grade] ||= []
+        @roster[grade] << name
+    end
+
+    def grade grade
+        @roster[grade]
+    end
+
+    def sort 
+        # @roster.sort_by{|grade, name_arr| name_arr[0]}.to_h
+        # @roster.sort.to_h
+        # @roster[grade].map{|name_arr| 
+        #     name_arr.sort
+        # }
+        @roster.map{|grade, name_arr|
+            name_arr.sort!
+        }
+        @roster
+
+    end
+
+end
+
+school = School.new("Bayside High School") 
+
+# binding.pry
